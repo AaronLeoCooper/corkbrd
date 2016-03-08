d7b8398a77ee0b3fc35a5eaf1c8ed5af894b9d61
AaronLeoCooper <aaron.cooper@2359media.com>
2016-03-08 23:39:21 +0800

    Basic skeleton in place

---
+++ README.md
@@ -0,0 +1,3 @@
+# Experimental 'corkboard' HTML layout creator
+
+This is a little plugin of sorts written in Javascript that allows adding/removing and serialising of HTML tags to and from JSON.
---
+++ scripts/corkbrd.js
@@ -0,0 +1,38 @@
+// Corkbrd isolated main script
+(function(window){
+
+  var defaults = {
+
+  };
+
+  // Init
+  this.Corkbrd = function(opts) {
+
+  };
+
+  Corkbrd.prototype.set = function() {
+
+  };
+
+  function extend(base, extend) {
+    for (var k in extend) {
+      base[k] = extend[k];
+    }
+    return base;
+  }
+
+  function genHTML() {
+
+  }
+
+})(window);
+
+window.onload = function() {
+
+  var opts = {
+
+  };
+
+  var cb = new Corkbrd(opts);
+
+};
